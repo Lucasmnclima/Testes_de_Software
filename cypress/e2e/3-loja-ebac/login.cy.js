5c8aa8780d57e99ea73f17a79e7b5a32a4240863
Lucas Nascimento <lucasmnclima@gmail.com>
2025-03-04 15:35:41 -0300

    primeira automatização de testes

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+// Referencia para o sistema entender que é um cypress code
+
+// Caminho feliz
+describe('Funcionalidade: Login na loja EBAC', () => {
+    
+    it('Deve fazer login com sucesso', () => {
+        // Acessar a página de login visita a url que queremos testar
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        // Inserir o email
+        cy.get('#username').type('lucas@teste.com.br')
+        // Inserir a senha
+        cy.get('#password').type('teste@123')
+        // Clicar no botão de login
+        cy.get('.woocommerce-form > .button').click('')
+        // Mensagem de login
+        // should é um método de validação. Está validando a mensagem de login correto
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, lucas (não é lucas? Sair)')
+    })
+
+    it('Em caso de digitar senha incorreta', () => {
+        // Acessar a página de login visita a url que queremos testar
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        // Inserir o email
+        cy.get('#username').type('lucas@teste.com.br')
+        // Inserir a senha incorreta
+        cy.get('#password').type('123458')
+        // Clicar no botão de login
+        cy.get('.woocommerce-form > .button').click('')
+        // Mensagem de erro
+        // should é um método de validação. Está validando a mensagem de login correto
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail lucas@teste.com.br está incorreta. Perdeu a senha?')
+
+    })
+})
